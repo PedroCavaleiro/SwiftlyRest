bac9e92255e1270f5f81adbfa6df059a882abbf7
Pedro Cavaleiro <pedro.cavaleiro@i-do.pt>
2025-05-15 15:04:58 +0100

    Removes unnecessary dependecy

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v15),
         .tvOS(.v15),
-        .macOS(.v14),
+        .macOS(.v12),
         .watchOS(.v6)
     ],
     products: [
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/evgenyneu/keychain-swift", .upToNextMajor(from: "24.0.0")),
-        .package(url: "https://github.com/PedroCavaleiro/ExtendedSwift", .upToNextMajor(from: "1.1.1")),
+        .package(url: "https://github.com/PedroCavaleiro/ExtendedSwift", .upToNextMajor(from: "1.1.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
